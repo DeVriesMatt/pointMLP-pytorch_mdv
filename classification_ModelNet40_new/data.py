d329bed5db16dc48e5286ea2923261d438508139
DeVriesMatt <matthew.devries.13@gmail.com>
2024-11-14 14:16:40 +0000

    updates

--- classification_ModelNet40_new/data.py
+++ classification_ModelNet40_new/data.py
@@ -20,7 +20,7 @@ def download():
 def load_data(partition):
     download()
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    DATA_DIR = os.path.join('/home/mvries/CurveNet', 'data')
     all_data = []
     all_label = []
     for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
